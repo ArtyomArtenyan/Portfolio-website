36d89e6c2079d0add96efa1014b2e8621b2a381c
ArtyomArtenyan <artyomartenyan.ait@gmail.com>
2024-12-29 13:14:05 +0400

    Finished Projects part

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { PROJECTS } from '../dataBase'; // Ensure this is imported correctly
 
 const Projects = () => {
-	return <div>Projects</div>;
+	return (
+		<div className='border-b border-neutral-900 pb-24'>
+			<h2 className='my-20 text-center text-4xl'>Projects</h2>
+			<div>
+				{PROJECTS.map((project, i) => (
+					<div key={i} className='flex mb-8 flex-wrap justify-center'>
+						<div className='w-1/4 max-w-md:w-full'>
+							<img
+								src={project.image}
+								alt={project.title}
+								className='rounded w-[150px] h-auto object-cover mb-6'
+							/>
+						</div>
+
+						<div className='w-3/4 max-w-xl max-w-md:w-full'>
+							<h6 className='font-semibold mb-2'>{project.title}</h6>
+							<p className=' text-neutral-400 mb-4'>{project.description}</p>
+							<div className='flex flex-wrap'>
+								{project.technologies.map((tech, index) => (
+									<span
+										key={index}
+										className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'
+									>
+										{tech}
+									</span>
+								))}
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
--- src/dataBase/index.js
+++ src/dataBase/index.js
@@ -1,7 +1,5 @@
-import project1 from '../assets/projects/project-1.jpg';
-import project2 from '../assets/projects/project-2.jpg';
-import project3 from '../assets/projects/project-3.jpg';
-import project4 from '../assets/projects/project-4.jpg';
+import project2 from '../assets/projects/project2.png';
+import project1 from '../assets/projects/prokect1.png';
 
 export const HERO_CONTENT =
 	'   Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam dolorem iste animi alias qui explicabo illo, asperiores accusamus modi aspernatur perspiciatis ratione mollitia recusandae quaerat dicta. Molestias recusandae doloremque nemo.';
@@ -34,32 +32,18 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
 	{
-		title: 'E-Commerce Website',
+		title: 'Website',
 		image: project1,
 		description:
 			'A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.',
-		technologies: ['HTML', 'CSS', 'React', 'Node.js', 'MongoDB'],
+		technologies: ['HTML', 'CSS', 'React'],
 	},
 	{
-		title: 'Task Management App',
+		title: 'Portfolio',
 		image: project2,
 		description:
-			'An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.',
-		technologies: ['HTML', 'CSS', 'Angular', 'Firebase'],
-	},
-	{
-		title: 'Portfolio Website',
-		image: project3,
-		description:
-			'A personal portfolio website showcasing projects, skills, and contact information.',
-		technologies: ['HTML', 'CSS', 'React', 'Bootstrap'],
-	},
-	{
-		title: 'Blogging Platform',
-		image: project4,
-		description:
-			'A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.',
-		technologies: ['HTML', 'CSS', 'Vue.js', 'Express', 'mySQL'],
+			'A personal portfolio website showcasing my projects and skills.',
+		technologies: ['JavaScript', 'TailwindCSS', 'Next.js'],
 	},
 ];
 
